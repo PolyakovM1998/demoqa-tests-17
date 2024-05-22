3898a13480ee55644715f771e533749e2ed11c84
mishk <mishka-polyakov007@maail.ru>
2024-05-23 00:13:18 +0300

    added pageobjects

--- src/test/java/demoqa/HomeWithPageObjectWorkDemoqa.java
+++ src/test/java/demoqa/HomeWithPageObjectWorkDemoqa.java
@@ -17,6 +17,12 @@ public class HomeWithPageObjectWorkDemoqa extends TestBase {
     @Test
     void homeWorkDemoqa(){
         String userName = "Misha";
+        String subjects = "Maths";
+        String hobbies = "Sports";
+        String currentaddress = "Perevoz";
+        String state = "NCR";
+        String city = "Delhi";
+        String submit = "Submit";
 
         registrationPage.openPage()
                 .setFirstName(userName)
@@ -24,24 +30,26 @@ public class HomeWithPageObjectWorkDemoqa extends TestBase {
                 .setEmail("mishka-polyakov007@mail.ru")
                 .setGender("Male")
                 .setPhone("1234567890")
-                .setBirthDate("30","July", "2008");
+                .setBirthDate("30","July", "2008")
+                .setSubjects(subjects)
+                .setHobbies(hobbies)
+                .setCurrentAdress(currentaddress)
+                .setState(state)
+                .setCity(city)
+                .setSubmit(submit);
 
-        $("#subjectsInput").setValue("Maths").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
+
  //       $("#uploadPicture").uploadFromClasspath(("img/1.png"));//вариант 44 строки
-        $("#currentAddress").setValue("Perevoz");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
-
         registrationPage.verifyResultsModalAppears()
                 .verifyResult("Student Name", userName + " Polyakov")
                 .verifyResult("Student Email", "mishka-polyakov007@mail.ru")
                 .verifyResult("Gender", "Male")
                 .verifyResult("Mobile", "1234567890")
-                .verifyResult("Date of Birth", "30 July,2008");
+                .verifyResult("Date of Birth", "30 July,2008")
+                .verifyResult("Subjects","Maths")
+                .verifyResult("Hobbies", "Sports")
+                .verifyResult("Address","Perevoz")
+                .verifyResult("State and City", state+" "+city);
 
     }
 }
